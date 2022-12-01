f8ddba243998062a7b8226f9f0e119a6031937d2
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2022-12-01 18:54:23 +0100

    Header and footer beginning

--- sito/templates/footer/footer.php
+++ sito/templates/footer/footer.php
@@ -1,3 +1,20 @@
-<footer>
-    footer
+<footer class="bg-dark text-center text-white">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Github -->
+        <section class="mb-4">
+            <button class="btn btn-outline-light btn-floating m-1" href="#!" role="button"></button>
+        </section>
+        <!-- Section: Social media -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:
+        <a class="text-white" href="http://u-niversity.ooguy.com/">U-niversity</a>
+        </br>
+        <a class="text-white" href="#">Informativa privacy</a>
+    </div>
+    <!-- Copyright -->
 </footer>
--- sito/templates/header/header.php
+++ sito/templates/header/header.php
@@ -1,3 +1,37 @@
 <header>
-    header
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="container-fluid">
+            <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="md-auto order-0">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".dual-collapse2">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="navbar-collapse collapse order-0 order-md-0 dual-collapse2">
+                <form class="d-flex">
+                    <input type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Messaggi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Notifiche</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Impostazioni</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
