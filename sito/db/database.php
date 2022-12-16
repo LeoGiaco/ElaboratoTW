b66125b99d059b690de9b3158f011267e9f26459
Fabio <fabio.notaro2@studio.unibo.it>
2022-12-16 22:33:43 +0100

    Translated error messages into Italian

--- sito/db/database.php
+++ sito/db/database.php
@@ -5,7 +5,7 @@
         public function __construct($servername, $username, $password, $dbname, $port){
             $this->db = new mysqli($servername, $username, $password, $dbname, $port);
             if ($this->db->connect_error) {
-                die("Connection failed: " . $this->db->connect_error);
+                die("Connessione al database fallita: " . $this->db->connect_error);
             }        
         }
 
