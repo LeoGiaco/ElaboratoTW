480aacbd9f7c139186af60cae4fc0934b9e873f4
LeoGiaco <leogiaco2@gmail.com>
2022-12-21 20:22:42 +0100

    Add removed class

--- sito/utils/js/function.js
+++ sito/utils/js/function.js
@@ -163,7 +163,7 @@ function visualizzaPost(numeroPost, aggiuntaPost=false, utente="", checked=false
             }
             row += `<p class="mt-3 mb-2 pb-2">${dati[i]["Testo"]}</p>
                     <div class="small d-flex justify-content-start">
-                        <button id="btnLike${dati[i]["ID"]}" class="d-flex align-items-center me-3 btn-3 btn-transition-up btn-sm" data-type="like" data-numero="${dati[i]["ID"]}" >Like: ${like}</button>
+                        <button id="btnLike${dati[i]["ID"]}" class="d-flex align-items-center me-3 btn btn-3 btn-transition-up btn-sm" data-type="like" data-numero="${dati[i]["ID"]}" >Like: ${like}</button>
                         <button id="btnDislike${dati[i]["ID"]}" class="d-flex align-items-center me-3 btn btn-4 btn-transition-down btn-sm" data-type="dislike" data-numero="${dati[i]["ID"]}">Dislike: ${dislike}</button>
                         <button class="d-flex align-items-center me-3 btn btn-1 btn-sm" data-type="commento" data-numero="${dati[i]["ID"]}">Commento</button>
                     </div>
