9f72c59794d46cfb087043203c2353192d0a85da
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2022-12-27 01:52:54 +0100

    change library

--- sito/templates/base.php
+++ sito/templates/base.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/tripledes.min.js" integrity="sha512-wmbsASIfkWYU3hcUOVevdepQTGGjbvjI0/cQ2xbHKwNdX+UKCrLUO4vA89Tlj4+RUu0putxrWOazYVwJcl2nUw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdn.jsdelivr.net/npm/crypto-js@4.1.1/tripledes.js%22%3E</script>
     <?php foreach($templateParams["javascript"] as $path):?>
         <script src="<?=$path?>"></script>
     <?php endforeach; ?>
